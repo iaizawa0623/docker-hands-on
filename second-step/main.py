664e0b0df54b3454e78630561c5147bcd2805f3f
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2022-06-13 11:51:16 +0900

    second-stop ok

--- second-step/main.py
+++ second-step/main.py
@@ -7,12 +7,25 @@ class ItemDataModel(BaseModel):
 	data: str
 
 app = FastAPI()
-db = MongoClient('mongo', '27017').sample_db
+db = MongoClient('mongo', 27017).sample_db
 
 @app.get("/")
-async def hello():
+async def health():
+	return "ok"
+
+@app.get("/items")
+async def list_items():
+	posts = {}
+	for post in db.posts.find():
+		posts[str(post['id'])] = post['data']
+	return posts
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str):
+	item = db.posts.find_one({"id": item_id})
 	return {
-		"message", "hello"
+		'id': item['id'],
+		'data': item['data']
 	}
 
 @app.post("/items/{item_id}")
@@ -26,9 +39,11 @@ async def post_item(item_id: str, item: ItemDataModel):
 		'message': 'ok'
 	}
 
-@app.get("/items")
-async def list_items():
-	posts = {}
-	for post in db.posts.find():
-		posts[str(post['id'])] = post['data']
-	return posts
+@app.put("/items/{item_id}")
+async def put_item(item_id: str, item: ItemDataModel):
+	return post_item(item_id, item)
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: str):
+	return db.posts.delete_one({"id": item_id})
+
